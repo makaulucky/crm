2859591af2011117d565fbb9743d8d13ad7ac978
makaulucky <makaulucky5@gmail.com>
2023-08-15 17:14:13 +0300

    Initial commit

---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+
+def products(request):
+    return render(request, 'accounts/products.html')
+
+def customers(request):
+    return render(request, 'accounts/customer.html')
+
+def accounts(request):
+    return render(request, 'accounts/accounts.html')
+
