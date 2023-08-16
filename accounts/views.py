6feaf59b99c21fd4613c076255d1f952f71c22b7
makaulucky <makaulucky5@gmail.com>
2023-08-16 16:51:28 +0300

    Work in progress

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Customer, Product, Order, Tag
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_orders = orders.count()
+    total_customers = customers.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    cancelled = orders.filter(status='Out for delivery').count()
+
+    context = {
+        'orders': orders,
+        'customers': customers,
+        'total_orders': total_orders,
+        'total_customers': total_customers,
+        'delivered': delivered,
+        'pending': pending,
+        'cancelled': cancelled
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 def customers(request):
     return render(request, 'accounts/customer.html')
 
 def accounts(request):
     return render(request, 'accounts/accounts.html')
-
