e2a769f76fdb9e46d77c174f7a347ab436e15cbd
makaulucky <makaulucky5@gmail.com>
2023-08-20 19:13:33 +0300

    Initial commit

---
+++ accounts/decorators.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+
+def unauthenticated_user(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            return view_func(request, *args, **kwargs)
+    return wrapper_func
+
+
+def allowed_users(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwags):
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+            if group in allowed_roles:
+                return view_func(request, *args, **kwags)
+            else:
+                return HttpResponse('You are not authorized to view this page')
+            return view_func(request, *args, **kwags)
+        return wrapper_func
+    return decorator
+
+
+
+def admin_only(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwags):
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+
+            if group == 'customer':
+                return redirect('userPage')
+            
+            if group == 'admin':
+                return view_func(request, *args, **kwags)
+        return wrapper_func
+    return decorator
