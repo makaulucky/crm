562c4d5e4a92a5d400e13d5a001e6e2386790554
makaulucky <makaulucky5@gmail.com>
2023-08-21 15:23:55 +0300

    Initial commit

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
-
+from django.urls import reverse
 
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
@@ -10,34 +10,30 @@ def unauthenticated_user(view_func):
             return view_func(request, *args, **kwargs)
     return wrapper_func
 
-
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
-        def wrapper_func(request, *args, **kwags):
+        def wrapper_func(request, *args, **kwargs):
             group = None
             if request.user.groups.exists():
                 group = request.user.groups.all()[0].name
+
             if group in allowed_roles:
-                return view_func(request, *args, **kwags)
+                return view_func(request, *args, **kwargs)
             else:
-                return HttpResponse('You are not authorized to view this page')
-            return view_func(request, *args, **kwags)
+                return HttpResponse("You are not authorized to view this page.")
         return wrapper_func
     return decorator
 
-
-
-def admin_only(allowed_roles=[]):
-    def decorator(view_func):
-        def wrapper_func(request, *args, **kwags):
-            group = None
-            if request.user.groups.exists():
-                group = request.user.groups.all()[0].name
-
-            if group == 'customer':
-                return redirect('userPage')
+def admin_only(view_func):
+    def wrapper_function(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
             
-            if group == 'admin':
-                return view_func(request, *args, **kwags)
-        return wrapper_func
-    return decorator
+        if group == 'Customer':
+            return redirect('user')
+        elif group == 'Admin':
+            return view_func(request, *args, **kwargs)
+        else:
+            return HttpResponse("You are not authorized to view this page.")
+    return wrapper_function
